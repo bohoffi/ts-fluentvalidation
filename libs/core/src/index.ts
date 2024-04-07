3f91bad53fe2002757fa94419667a3d53bfb8184
bohoffi <bo.hoffi@gmail.com>
2024-04-07 12:59:58 +0200

    refactor: expose lib version

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,3 +1,4 @@
 export { createValidator, Validator, ValidatorInitFn } from './lib/validator';
 export { ValidationFailure, ValidationResult } from './lib/result';
 export { ValidatorConfig } from './lib/models';
+export { VERSION } from './lib/version';
